cba5627c55a2eebbe6a46c606d58fc391895ef26
yar muhammad <76510029+Yarmuhammadtalpur@users.noreply.github.com>
2023-09-24 23:03:08 +0500

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This app working.. or not!</Text>
+      <Text>Yai! app working.. or not!</Text>
       <StatusBar style="auto" />
     </View>
   );
